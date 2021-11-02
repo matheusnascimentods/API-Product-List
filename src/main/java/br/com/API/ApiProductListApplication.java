15044eefaf926af45514a458f32c0909ea4cf29a
Matheus-dev58 <matheus.nita78@gmail.com>
2021-11-02 11:22:52 -0300

    O projeto foi criado

---
+++ src/main/java/br/com/API/ApiProductListApplication.java
@@ -0,0 +1,13 @@
+package br.com.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProductListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProductListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/API/ApiProductListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProductListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

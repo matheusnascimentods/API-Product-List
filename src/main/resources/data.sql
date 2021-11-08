eb2a70d0e5219c88044b53c2bdc976ea09bd40ec
Matheus-dev58 <matheus.nita78@gmail.com>
2021-11-07 22:43:30 -0300

    O arquivo data.sql foi criado para ter produtos já cadastrados no sistema

---
+++ src/main/resources/data.sql
@@ -0,0 +1,121 @@
+INSERT INTO PRODUCT(nome, preco, categoria, url, img) 
+	VALUES(
+		
+			'Mouse Gamer Logitech G203 LIGHTSYNC RGB',
+			114.90,
+			'Computador',
+			'https://www.amazon.com.br/gp/product/B087CT8PWY/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1',
+			'https://m.media-amazon.com/images/I/71OrygkkeOL._AC_SL1500_.jpg'
+			
+		);
+		
+INSERT INTO PRODUCT(nome, preco, categoria, url, img) 
+	VALUES(
+		
+			'The Witcher 3 - Complete Edition - Xbox One',
+			130.89,
+			'Games e consoles',
+			'https://www.amazon.com.br/gp/product/B07M7PDPY7?pf_rd_r=QV94XEVNTTXT4K0MSKG3&pf_rd_p=ee4657d9-619b-45ec-becf-9c00a63ba6a0&pd_rd_r=48cf5078-d207-4570-97f5-b87df380cf58&pd_rd_w=991sX&pd_rd_wg=r70RR&ref_=pd_gw_unk',
+			'https://m.media-amazon.com/images/I/815DYbxB+SL._AC_SL1500_.jpg'
+			
+		);
+		
+INSERT INTO PRODUCT(nome, preco, categoria, url, img) 
+	VALUES(
+		
+			'Assassins Creed Valhalla - Edição Limitada - Xbox One',
+			189.89,
+			'Games e consoles',
+			'https://www.americanas.com.br/produto/2086828086?pfm_carac=assassin-s-creed-valhalla-edicao-limitada-xbox-one&pfm_page=search&pfm_pos=grid&pfm_type=search_page&offerId=5f6a48d31b186381b5fd3cbf',
+			'https://images-americanas.b2w.io/produtos/01/00/img/2086828/1/2086828107_2SZ.jpg'
+			
+		);
+		
+INSERT INTO PRODUCT(nome, preco, categoria, url, img) 
+	VALUES(
+		
+			'Tênis Nike Venture Runner Masculino',
+			239.99,
+			'Moda',
+			'https://www.netshoes.com.br/tenis-nike-venture-runner-masculino-azul+navy-HZM-3712-C69',
+			'https://static.netshoes.com.br/produtos/tenis-nike-venture-runner-masculino/69/HZM-3712-C69/HZM-3712-C69_zoom1.jpg?ts=1585336447&'
+			
+		);
+		
+INSERT INTO PRODUCT(nome, preco, categoria, url, img) 
+	VALUES(
+		
+			'Batedeira Mondial Preto 400W Prática B-44-B - 3 Velocidades',
+			89.90,
+			'Cozinha',
+			'https://www.magazineluiza.com.br/batedeira-mondial-preto-400w-pratica-b-44-b-3-velocidades/p/021289200/ep/bate/?&seller_id=magazineluiza&utm_source=google&utm_medium=pla&utm_campaign=&partner_id=58949&gclsrc=ds&gclsrc=ds',
+			'https://a-static.mlcdn.com.br/1500x1500/batedeira-mondial-preto-400w-pratica-b-44-b-3-velocidades/magazineluiza/021289200/f6117622a0d18f31a4864638c09b217d.jpg'
+			
+		);
+		
+INSERT INTO PRODUCT(nome, preco, categoria, url, img) 
+	VALUES(
+		
+			'Escrivaninha Stanford Carvalho e Preta 100 cm',
+			409.99,
+			'Movel',
+			'https://www.mobly.com.br/escrivaninha-stanford-carvalho-e-preta-100-cm-800659.html?spall_source=especiais',
+			'https://staticmobly.akamaized.net/p/Keva-Escrivaninha-Stanford-Carvalho-e-Preta-100-cm-8396-956008-1-zoom.jpg'
+			
+		);
+		
+INSERT INTO PRODUCT(nome, preco, categoria, url, img) 
+	VALUES(
+		
+			'Percy Jackson E Os Olimpianos:  O Ladrão De Raios',
+			37.40,
+			'Livro',
+			'https://www.americanas.com.br/produto/120120314?epar=bp_pl_00_go_liv_todas_geral_gmv&opn=YSMESP&WT.srch=1',
+			'https://images-americanas.b2w.io/produtos/imagens/120120314/120120322_1SZ.jpg'
+			
+		);
+		
+INSERT INTO PRODUCT(nome, preco, categoria, url, img) 
+	VALUES(
+		
+			'Smart TV LED 55" 4K UHD LG 55UN731C, 3 HDMI, 2 USB, Wi-Fi, Assistente Virtual e Bluetooth',
+			2808.89,
+			'Eletrônico',
+			'https://www.amazon.com.br/dp/B08FJC83S4/ref=s9_acsd_hps_bw_c2_x_2_i?pf_rd_m=A3RN7G7QC5MWSZ&pf_rd_s=merchandised-search-5&pf_rd_r=FY1RHVR58H3MZ51MCM75&pf_rd_t=101&pf_rd_p=f672194a-4b00-4b39-a9da-977565dd501d&pf_rd_i=16209062011',
+			'https://m.media-amazon.com/images/I/61x3zXEwIXL._AC_SL1000_.jpg'
+			
+		);
+		
+INSERT INTO PRODUCT(nome, preco, categoria, url, img) 
+	VALUES(
+		
+			'LEGO Star Wars - Capacete de Darth Vader, 834 Peças - 75304',
+			332.49,
+			'Brinquedo',
+			'https://www.kabum.com.br/produto/195037/lego-star-wars-capacete-de-darth-vader-834-pecas-75304',
+			'https://images.kabum.com.br/produtos/fotos/195037/lego-star-wars-capacete-de-darth-vader-834-pecas-75304_1628596339_gg.jpg'
+			
+		);
+		
+INSERT INTO PRODUCT(nome, preco, categoria, url, img) 
+	VALUES(
+		
+			'LEGO Star Wars - Imperial TIE Fighter, 432 Peças - 75300',
+			332.49,
+			'Brinquedo',
+			'https://images.kabum.com.br/produtos/fotos/148314/lego-star-wars-imperial-tie-fighter-432-pecas-75300_1612877475_gg.jpg',
+			'https://www.kabum.com.br/produto/148314/lego-star-wars-imperial-tie-fighter-432-pecas-75300'
+			
+		);
+		
+INSERT INTO PRODUCT(nome, preco, categoria, url, img) 
+	VALUES(
+		
+			'Jogo War Vikings Grow + Pacote 100 Sleeves Mayday Standard USA',
+			129.99,
+			'Jogo de tabuleiro',
+			'https://www.amazon.com.br/War-Vikings-Grow-Sleeves-Standard/dp/B09BQ1CZSM/ref=sr_1_2?keywords=war+vikings&qid=1636334514&s=toys&sprefix=war%2Ctoys%2C1007&sr=1-2&ufe=app_do%3Aamzn1.fos.6a09f7ec-d911-4889-ad70-de8dd83c8a74',
+			'https://m.media-amazon.com/images/I/71ST6bycx3L._AC_SL1494_.jpg'
+			
+		);
+	
